15a464e4be263f954126bd19c7eb149cb7c15976
KasterborousConstellation <paul.andrieu28@gmail.com>
2025-07-09 22:26:36 +0200

    navbar style

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,6 +12,6 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(Request $request): Response
     {
-        return $this->render("home/index.html.twig",["items" => ["Home","Home","Home","Home"]]);
+        return $this->render("home/index.html.twig");
     }
 }
---
+++ src/Controller/LessonController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class LessonController extends AbstractController
+{
+    #[Route('/lesson', name: 'lessons')]
+    public function index(): Response
+    {
+        return $this->render("lesson/index.html.twig");
+    }
+}
