2d57af008e215abd3d6ecb53e7ddfa35b2f4b8ba
KasterborousConstellation <paul.andrieu28@gmail.com>
2025-07-10 16:05:06 +0200

    contact

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'contact')]
+    public function index(): Response
+    {
+        return $this->render('contact/index.html.twig');
+    }
+
+    #[Route('/contact/mail',name: 'contact.mail')]
+    public function mail() : Response
+    {
+        return $this->render('contact/mail.html.twig');
+    }
+}
