9f8d42a61a67922d8e7881d104c4038a68d1db50
BuildTools <paul.andrieu28@gmail.com>
2025-09-11 19:28:53 +0200

    user Add admin panel and user management

--- src/Controller/AdminPanelController.php
+++ src/Controller/AdminPanelController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,8 +14,21 @@ final class AdminPanelController extends AbstractController
     public function index(): Response
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
-        return $this->render('admin_panel/index.html.twig', [
-            'controller_name' => 'AdminPanelController',
-        ]);
+        return $this->render('admin_panel/index.html.twig');
+    }
+    #[Route('/admin/users', name: 'app_admin_users')]
+    public function users(EntityManagerInterface $em): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        $users = $em->getRepository('App\Entity\User')->findAll();
+        $users = array_map(function($user) {
+            return [
+                'username' => $user->getUserName(),
+                'email' => $user->getEmail(),
+                'roles' => $user->getRoles(),
+                'createdAt' => $user->getCreatedAt()->format('Y-m-d H:i:s'),
+            ];
+        }, $users);
+        return $this->render('admin_panel/users.html.twig',["users" => $users]);
     }
 }
